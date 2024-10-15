42dbc29cdf763cc85b8ece2920275fe1e45dd56f
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-10-15 19:07:36 -0300

    emitting events globally
    Handle with input value from client (front-end), sending to server (back-end) to send it back to all sockets connected and changing text value real time

--- public/document.js
+++ public/document.js
@@ -5,3 +5,7 @@ const textEditor = document.getElementById("text-editor");
 textEditor.addEventListener("keyup", () => {
   socket.emit("keyupEvent", textEditor.value);
 });
+
+socket.on("text_value_server_to_client", (arg) => {
+  textEditor.value = arg;
+});
--- src/server.js
+++ src/server.js
@@ -21,11 +21,8 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
   console.log("a user connected");
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
   socket.on("keyupEvent", (text) => {
-    console.log(text);
+    socket.broadcast.emit("text_value_server_to_client", text); // I could use io.emit() to send this event to all clients, but I need to send to all but this one that is emitting. I am getting the message from Front-end and sending back to all sockets connected
   });
 });
 
