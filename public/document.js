cb4a3a9b8964aa05bbc1f27b490efd2cb12e9e73
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-10-18 23:41:09 -0300

    refactoring rooms events
    sockets functions refactored and easy to understand. Separating event to get document name and the key up event

--- public/document.js
+++ public/document.js
@@ -1,14 +1,14 @@
 import { socketEmitEvent } from "./document-socket.js";
 
 const textEditor = document.getElementById("text-editor");
-const documentName = new URLSearchParams(window.location.search).get("name");
+const documentName = new URLSearchParams(window.location.search).get("name"); // Example: window.location.search = "?name=JavaScript". URLSearchParams will provide methods to interact to each params of my URL (in this case to get document name value)
 const documentTitle = document.getElementById("document-title");
 documentTitle.innerText = documentName;
 
 socketEmitEvent("documentName", documentName);
 
 textEditor.addEventListener("keyup", () => {
-  socketEmitEvent("keyupEvent", textEditor.value);
+  socketEmitEvent("keyupEvent", { text: textEditor.value, documentName });
 });
 
 export function updateTextEditor(text) {
--- src/server-socket.js
+++ src/server-socket.js
@@ -1,7 +1,7 @@
 import { io } from "./server.js";
 
 const documentText = {
-  JavaScript: "",
+  JavaScript: "Javas",
   Node: "",
   "Socket.io": "",
 };
@@ -11,16 +11,28 @@ io.on("connection", (socket) => {
 
   socket.on("documentName", (arg) => {
     socket.join(arg);
-    socket.nsp.to(arg).emit("textLoadedServerToClient", documentText[arg]);
 
-    console.log("Já emiti", documentText[arg]);
-    socket.on("keyupEvent", (text) => {
-      documentText[arg] = text;
-      socket.to(arg).emit("text_value_server_to_client", text); // I could use io.emit() to send this event to all clients, but I need to send to all but this one that is emitting. I am getting the message from Front-end and sending back to all sockets connected. Another detail is that I am doing it just for socket connected at the same room
-    });
+    const document = findDocument(arg);
+    if (document) {
+      socket.nsp.to(arg).emit("textLoadedServerToClient", documentText[arg]); // I could use io.to(arg)
+    }
+  });
+
+  socket.on("keyupEvent", ({ text, documentName }) => {
+    const document = findDocument(arg);
+    if (document) {
+      documentText[documentName] = text;
+      socket.to(documentName).emit("text_value_server_to_client", text); // I could use io.emit() to send this event to all clients, but I need to send to all but this one that is emitting. I am getting the message from Front-end and sending back to all sockets connected. Another detail is that I am doing it just for socket connected at the same room
+    }
   });
 
   socket.on("disconnect", (msg) => {
     // console.log(`user disconnected. ID: ${socket.id}. Message: ${msg}`);
   });
 });
+
+function findDocument(documentName) {
+  return Object.keys(documentText).find((name) => {
+    return name === documentName;
+  });
+}
