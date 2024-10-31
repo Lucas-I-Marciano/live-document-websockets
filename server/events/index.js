c710e54d9fb3c9885f183d9fec25ab4d9af93701
Lucas-I-Marciano <lucas.marciano99@outlook.com>
2024-10-30 21:40:44 -0300

    :books: docs: refactoring back-end events
    events folder created to handle with events coming from HTML file (front-end)

---
+++ server/events/index.js
@@ -0,0 +1,29 @@
+import {
+  findDocument,
+  getAllDocuments,
+  createDocument,
+} from "../db/operations.js";
+
+// Index socket
+export function indexEvents(socket, io) {
+  socket.on("loadIndex", async () => {
+    const cursor = await getAllDocuments();
+    cursor.forEach((document) => {
+      socket.emit("documentNameToIndex", document["name"]);
+    });
+  });
+
+  socket.on("addDocument", async (documentName) => {
+    const isDocumentedCreated = await findDocument(documentName);
+    if (!isDocumentedCreated) {
+      const result = await createDocument(documentName);
+      if (result["acknowledged"]) {
+        io.emit("documentCreated");
+      } else {
+        io.emit("documentFailedCreation");
+      }
+    } else {
+      io.emit("documentAlreadyCreated", documentName);
+    }
+  });
+}
